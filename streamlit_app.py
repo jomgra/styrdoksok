f4f3c06bb86d24e9e4919042817e5fc1aaf21232
jomgra <joagr@icloud.com>
2022-12-29 14:40:09 +0100

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,12 +7,12 @@ st.title('Sök instruktioner')
 myn_reg = ('https://myndighetsregistret.scb.se/myndighet/download?myndgrupp=Statliga%20förvaltningsmyndigheter&format=True')
 
 def load_data(url):
-		web = req.get(url)
-		web.encoding = web.apparent_encoding
-    data = pd.read_excel(web)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    return data
+	web = req.get(url)
+	web.encoding = web.apparent_encoding
+	data = pd.read_excel(web)
+	lowercase = lambda x: str(x).lower()
+	data.rename(lowercase, axis='columns', inplace=True)
+	return data
 
 # Create a text element and let the reader know the data is loading.
 data_load_state = st.text('Loading data...')
