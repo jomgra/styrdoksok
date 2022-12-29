a08decff4542357030f0fa09de9cde70c2a79446
jomgra <joagr@icloud.com>
2022-12-29 14:07:58 +0100

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,19 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Sök intruktioner')
+st.title('Sök instruktioner')
 
+myn_reg = ('https://myndighetsregistret.scb.se/myndighet/download?myndgrupp=Statliga%20förvaltningsmyndigheter&format=False')
 
+def load_data(url):
+    data = pd.read_csv(url)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    return data
+
+# Create a text element and let the reader know the data is loading.
+data_load_state = st.text('Loading data...')
+# Load 10,000 rows of data into the dataframe.
+data = load_data(url)
+# Notify the reader that the data was successfully loaded.
+data_load_state.text('Loading data...done!')
