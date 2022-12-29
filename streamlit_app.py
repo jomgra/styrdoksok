a071aefad893613ddf57bb5964611c76b164af7c
jomgra <joagr@icloud.com>
2022-12-29 15:45:39 +0100

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,20 +8,20 @@ st.title('Sök instruktioner')
 
 myn_reg = ('https://myndighetsregistret.scb.se/myndighet/download?myndgrupp=Statliga%20förvaltningsmyndigheter&format=True')
 
-@st.cache
 def load_data(url):
-	web = req.get(url)
-	web.encoding = web.apparent_encoding
-	data = pd.read_excel(web.content)
+	data = pd.read_excel(webload(url))
 	lowercase = lambda x: str(x).lower()
 	data.rename(lowercase, axis='columns', inplace=True)
 	return data
 
-# Create a text element and let the reader know the data is loading.
+@st.cache
+def webload(url):
+	web = req.get(url)
+	web.encoding = web.apparent_encoding
+	return web.content
+
 data_load_state = st.text('Loading data...')
-# Load 10,000 rows of data into the dataframe.
 data = load_data(myn_reg)
-# Notify the reader that the data was successfully loaded.
 data_load_state.text('Loading data...done!')
 
 st.subheader('Raw data')
