a86d2679a02a69fd41aa277f90e244abb0b5f149
jomgra <joagr@icloud.com>
2022-12-29 14:09:17 +0100

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,6 @@ def load_data(url):
 # Create a text element and let the reader know the data is loading.
 data_load_state = st.text('Loading data...')
 # Load 10,000 rows of data into the dataframe.
-data = load_data(url)
+data = load_data(myn_reg)
 # Notify the reader that the data was successfully loaded.
 data_load_state.text('Loading data...done!')
