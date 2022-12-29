f146ac794ae9bf0b45b0a92d98f17a44c79034f9
jomgra <joagr@icloud.com>
2022-12-29 14:52:17 +0100

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,7 @@ st.title('Sök instruktioner')
 
 myn_reg = ('https://myndighetsregistret.scb.se/myndighet/download?myndgrupp=Statliga%20förvaltningsmyndigheter&format=True')
 
+@st.cache
 def load_data(url):
 	web = req.get(url)
 	web.encoding = web.apparent_encoding
@@ -22,3 +23,8 @@ data_load_state = st.text('Loading data...')
 data = load_data(myn_reg)
 # Notify the reader that the data was successfully loaded.
 data_load_state.text('Loading data...done!')
+
+st.subheader('Raw data')
+st.write(data)
+
+
