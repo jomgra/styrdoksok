70931e4dedac2d6b67ca8af1769f1b1ecc837b10
jomgra <joagr@icloud.com>
2023-01-01 17:07:44 +0100

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ def webload(url):
 @st.cache(persist=True)
 def load_doc(url, typ):
 	if not validators.url(str(url)):
-		st.write(url)
+		st.write('.' + url + '.')
 		return None
 	time.sleep(3)
 	html = webload(url)
